8f238b70c62d234b996daa4ac5ef09e9aba80c44
User <User@WINDEV2311EVAL>
2023-12-04 09:16:38 -0800

    Add project files.

---
+++ LAbs_cz/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace LAbs_cz
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
